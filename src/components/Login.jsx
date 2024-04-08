5ca16f90d05872f6eaf313a24642626fe1b21972
Marina Matulle <marinamatulle.mm@gmail.com>
2024-04-07 22:41:44 -0300

    creates login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,66 @@
-import React from 'react';
-import { ReactComponent as AstromeowSVG } from '../assets/astromeow.svg';
+import React from "react";
+import { ReactComponent as AstromeowSVG } from "../assets/astromeow.svg";
+import { ReactComponent as GmailSVG } from "../assets/gmail-google.svg";
 
 const Login = () => {
   return (
     <div className="flex w-full h-screen">
       <div className="w-full flex items-center justify-center lg:w-1/2">
-        <div>Login</div>
+        <div className="bg-white px-10 py-20 rounded-3xl border-2 border-gray-200">
+          <h1 className="text-5xl font-semibold">Bem-vindo de volta!</h1>
+          <p className="font-medium text-lg text-gray-500 mt-4">
+            Digite seu email e senha para entrar no Petslife.
+          </p>
+          <div className="mt-8">
+            <div>
+              <label className="text-lg font-medium">Email:</label>
+              <input
+                className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
+                placeholder="Digite seu email"
+              />
+            </div>
+            <div>
+              <label className="text-lg font-medium">Senha:</label>
+              <input
+                className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
+                placeholder="Digite sua senha"
+                type="password"
+              />
+            </div>
+            <div className="mt-8 flex justify-between items-center">
+              <div>
+                <input type="checkbox" id="remember" />
+                <label className="ml-2 font-medium text-base" for="remember">
+                  Lembre-se de mim
+                </label>
+              </div>
+              <button className="font-medium text-base text-orange-400">
+                Esqueci minha senha
+              </button>
+            </div>
+            <div className="mt-8 flex flex-col gap-y-4">
+              <button className="active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out py-3 rounded-xl bg-orange-400 text-white text-lg font-bold">
+                Entrar
+              </button>
+              <button className="active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out py-3 rounded-xl flex border-2 border-gray-100 items-center justify-center text-lg">
+                <GmailSVG alt="Gmail logo" className="w-6 h-6 mr-2" />
+                Entrar com Google
+              </button>
+            </div>
+            <div className="mt-8 flex justify-center items-center">
+              <p className="fonte-medium text-base">Ainda não tem uma conta?</p>
+              <button className="text-orange-400 text-base font-medium ml-2">
+                Registre-se
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="hidden relative lg:flex h-full w-1/2 items-center justify-center bg-gray-200">
-       
-        <AstromeowSVG alt='Astromeow Image' />
-        
+        <AstromeowSVG
+          alt="Astromeow gato astronauta imagem"
+          className=""
+        />
       </div>
     </div>
   );
