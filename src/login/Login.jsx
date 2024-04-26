514d3d36470585dff2eae4fbd17cf0c37eae576e
Marina Matulle <marinamatulle.mm@gmail.com>
2024-04-26 20:34:01 -0300

    feat: adds firebase auth to login

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+import { toast } from "react-toastify";
+
 import { ReactComponent as PetsSVG } from "../assets/cat-and-dog.svg";
 import { ReactComponent as GmailSVG } from "../assets/gmail-google.svg";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      window.location.href = "/profile";
+      toast.success("Logado com sucesso!", {
+        position: "top-center",
+      });
+    } catch (error) {
+      console.error(error.message);
+      toast.error(error.message, {
+        position: "bottom-center",
+      });
+    }
+  };
+
   return (
     <div className="flex w-full min-h-screen">
       <div className="w-full flex items-center justify-center lg:w-1/2">
@@ -13,39 +36,48 @@ const Login = () => {
             Digite seu email e senha para entrar no Petslife.
           </p>
           <div className="mt-8">
-            <div>
-              <label className="text-lg font-medium">Email:</label>
-              <input
-                className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
-                placeholder="Digite seu email"
-              />
-            </div>
-            <div>
-              <label className="text-lg font-medium">Senha:</label>
-              <input
-                className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
-                placeholder="Digite sua senha"
-                type="password"
-              />
-            </div>
-            <div className="mt-8 flex flex-col gap-y-4">
-              <button className="active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out py-3 rounded-xl bg-orange-400 text-white text-lg font-bold">
-                Entrar
-              </button>
-              <button className="active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out py-3 rounded-xl flex border-2 border-gray-100 items-center justify-center text-lg">
-                <GmailSVG alt="Gmail logo" className="w-6 h-6 mr-2" />
-                Entrar com Google
-              </button>
-            </div>
-            <div className="mt-8 flex justify-center items-center">
-              <p className="fonte-medium text-base">Ainda não tem uma conta?</p>
-              <Link
-                to="/register"
-                className="text-orange-400 text-base font-medium ml-2"
-              >
-                Cadastre-se
-              </Link>
-            </div>
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label className="text-lg font-medium">Email:</label>
+                <input
+                  className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
+                  placeholder="Digite seu email"
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div>
+                <label className="text-lg font-medium">Senha:</label>
+                <input
+                  className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
+                  placeholder="Digite sua senha"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div className="mt-8 flex flex-col gap-y-4">
+                <button className="active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out py-3 rounded-xl bg-orange-400 text-white text-lg font-bold">
+                  Entrar
+                </button>
+                <button className="active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out py-3 rounded-xl flex border-2 border-gray-100 items-center justify-center text-lg">
+                  <GmailSVG alt="Gmail logo" className="w-6 h-6 mr-2" />
+                  Entrar com Google
+                </button>
+              </div>
+              <div className="mt-8 flex justify-center items-center">
+                <p className="fonte-medium text-base">
+                  Ainda não tem uma conta?
+                </p>
+                <Link
+                  to="/register"
+                  className="text-orange-400 text-base font-medium ml-2"
+                >
+                  Cadastre-se
+                </Link>
+              </div>
+            </form>
           </div>
         </div>
       </div>
