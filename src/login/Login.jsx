d493ee9fc08cbccb1cb65536f9e0355f455816e8
Marina Matulle <marinamatulle.mm@gmail.com>
2024-05-10 17:02:37 -0300

    fix: removes google auth

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
-import { auth, googleAuth, database } from "../firebase";
-import { doc, setDoc } from "firebase/firestore";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 import { toast } from "react-toastify";
 
 import { ReactComponent as PetsSVG } from "../assets/cat-and-dog.svg";
-import { ReactComponent as GmailSVG } from "../assets/gmail-google.svg";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -26,24 +24,6 @@ const Login = () => {
       toast.error("Email ou senha incorretos.", {
         position: "bottom-center",
       });
-    }
-  };
-
-  const handleGoogleSubmit = async () => {
-    try {
-      const result = await signInWithPopup(auth, googleAuth);
-      const user = result.user;
-      const userDocRef = doc(database, "GoogleRegularUsers", user.uid);
-      await setDoc(userDocRef, {
-        email: user.email,
-        fullName: user.displayName,
-      });
-      navigate("/profile")
-      toast.success("Logado com sucesso!", {
-        position: "top-center",
-      });
-    } catch (error) {
-      console.error(error.message);
     }
   };
 
@@ -80,10 +60,6 @@ const Login = () => {
               <div className="mt-8 flex flex-col gap-y-4">
                 <button className="active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out py-3 rounded-xl bg-orange-400 text-white text-lg font-bold">
                   Entrar
-                </button>
-                <button type="button" onClick={handleGoogleSubmit} className="active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out py-3 rounded-xl flex border-2 border-gray-100 items-center justify-center text-lg">
-                  <GmailSVG alt="Gmail logo" className="w-6 h-6 mr-2" />
-                  Entrar com Google
                 </button>
               </div>
               <div className="mt-8 flex justify-center items-center">
