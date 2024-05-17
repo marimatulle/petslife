d5c60a68877cc0e93e4f10056a08b887e5ea38fb
Marina Matulle <marinamatulle.mm@gmail.com>
2024-05-17 14:20:39 -0300

    fix: App.jsx rollback

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { auth } from "./firebase";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import Login from './login/Login';
-import Register from './register/Register';
-import Profile from './profile/Profile';
-import Cards from './cards/Cards';
+import Login from "./login/Login";
+import Register from "./register/Register";
+import Profile from "./profile/Profile";
+import Cards from "./cards/Cards";
 
 const App = () => {
-  const [user, setUser] = useState();
-  useEffect(() => {
-    auth.onAuthStateChanged(user => {
-      setUser(user);
-    });
-  });
   return (
     <div>
-        <Routes>
-          <Route path="/" element={user?<Navigate to="/profile"/>:<Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/cards" element={<Cards />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/cards" element={<Cards />} />
+      </Routes>
       <ToastContainer />
     </div>
   );
