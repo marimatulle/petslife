6b00de609de288433fc69b07c421d0a6d92ee702
Marina Matulle <marinamatulle.mm@gmail.com>
2024-05-19 23:27:29 -0300

    feat: implements the profile in SearchedUserProfile component

--- src/search/SearchedUserProfile.jsx
+++ src/search/SearchedUserProfile.jsx
@@ -3,12 +3,18 @@ import { useParams } from "react-router-dom";
 import { doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
 import { database, auth } from "../firebase";
 import Topbar from "../bars/Topbar";
+import { FaUserCircle } from "react-icons/fa";
+import ClipLoader from "react-spinners/ClipLoader";
+import { toast } from "react-toastify";
 
 const SearchedUserProfile = () => {
   const { userId } = useParams();
   const [user, setUser] = useState(null);
   const [friendshipStatus, setFriendshipStatus] = useState(null);
   const [currentUser, setCurrentUser] = useState(null);
+  const [isHovered, setIsHovered] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [buttonText, setButtonText] = useState("Enviar solicitação de amizade");
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -71,6 +77,7 @@ const SearchedUserProfile = () => {
         status: "pending",
       });
       setFriendshipStatus("pending");
+      toast.success("Sua solicitação de amizade foi enviada com sucesso!");
     }
   };
 
@@ -91,15 +98,55 @@ const SearchedUserProfile = () => {
   return (
     <div className="bg-gray-100 min-h-screen">
       <Topbar location="/userprofile/:userId" />
-      <p>{user?.username}</p>
-      <p>{user?.name}</p>
-      {friendshipStatus === "pending" ? (
-        <button onClick={unfriend}>Cancelar solicitação de amizade</button>
-      ) : (
-        <button onClick={sendFriendRequest}>
-          Enviar solicitação de amizade
-        </button>
-      )}
+      <div className="flex justify-center items-center h-full">
+        <div className="w-full lg:w-3/4 xl:w-1/2 bg-white shadow rounded-lg p-8 m-4">
+          <div className="flex justify-center">
+            <label
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+              className="relative"
+            >
+              {isLoading ? (
+                <ClipLoader color="#fb923c" loading={isLoading} size={150} />
+              ) : user?.photoURL ? (
+                <img
+                  className="w-48 h-48 rounded-full mx-auto"
+                  src={user.photoURL}
+                  alt="Avatar do usuário"
+                />
+              ) : (
+                <FaUserCircle className="text-gray-400 w-48 h-48 mx-auto" />
+              )}
+            </label>
+          </div>
+          <p className="text-center mt-3 font-bold text-2xl mb-2">
+            {user?.name}
+          </p>
+          <p className="text-center text-gray-400 text-lg">
+            {user?.username}
+            {user?.crmv ? ` (CRMV: ${user?.crmv})` : ""}
+          </p>
+          <div className="flex items-center justify-center mt-4 space-x-4">
+            {friendshipStatus === "pending" ? (
+              <button
+                onClick={unfriend}
+                className="bg-orange-300 hover:bg-orange-400 text-white font-bold py-2 px-4 rounded-xl active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out"
+              >
+                Cancelar solicitação de amizade
+              </button>
+            ) : (
+              <button
+                onClick={sendFriendRequest}
+                className="bg-orange-300 hover:bg-orange-400 text-white font-bold py-2 px-4 rounded-xl active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out"
+              >
+                {friendshipStatus === "pending"
+                  ? "Solicitação enviada"
+                  : "Enviar solicitação de amizade"}
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
