c1c0e6bad9f87feb314afe88ac941ff4d70c0c1b
Marina Matulle <marinamatulle.mm@gmail.com>
2024-05-20 23:25:21 -0300

    implements the friends list

--- src/profile/FriendsModal.jsx
+++ src/profile/FriendsModal.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { IoClose } from "react-icons/io5";
+import { collection, query, where, getDocs, doc, getDoc } from "firebase/firestore";
+import { database, auth } from "../firebase";
+import { Link } from "react-router-dom";
 
 const FriendsModal = ({ onClose }) => {
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    const fetchFriends = async () => {
+      const q = query(
+        collection(database, "FriendRequests"),
+        where("receiverId", "==", auth.currentUser.uid),
+        where("status", "==", "accepted")
+      );
+      const querySnapshot = await getDocs(q);
+      const friendsPromises = querySnapshot.docs.map(async (docSnapshot) => {
+        const senderId = docSnapshot.data().senderId;
+        const senderDocRef = doc(database, "RegularUsers", senderId);
+        const senderDocSnap = await getDoc(senderDocRef);
+        if (senderDocSnap.exists()) {
+          return {
+            id: docSnapshot.id,
+            friend: senderDocSnap.data(),
+            senderId: senderId,
+            ...docSnapshot.data(),
+          };
+        } else {
+          const senderDocRef = doc(database, "Veterinarians", senderId);
+          const senderDocSnap = await getDoc(senderDocRef);
+          if (senderDocSnap.exists()) {
+            return {
+              id: docSnapshot.id,
+              friend: senderDocSnap.data(),
+              senderId: senderId, // Adicione esta linha
+              ...docSnapshot.data(),
+            };
+          }
+        }
+      });
+      const friends = await Promise.all(friendsPromises);
+      setFriends(friends);
+    };
+
+    fetchFriends();
+  }, []);
+
   return (
     <div
       className="fixed z-10 inset-0 overflow-y-auto"
@@ -17,18 +61,38 @@ const FriendsModal = ({ onClose }) => {
         <span
           className="hidden sm:inline-block sm:align-middle sm:h-screen"
           aria-hidden="true"
-        >
-        </span>
-        <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+        ></span>
+        <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-md sm:w-full">
           <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
             <div className="sm:flex sm:items-start">
-              <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+              <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left w-full">
                 <h3
                   className="text-lg leading-6 font-bold text-gray-900"
                   id="modal-title"
                 >
                   Amigos:
                 </h3>
+                {friends.map((friend) => (
+                  <Link to={`/userprofile/${friend.senderId}`} key={friend.id}>
+                    {" "}
+                    {/* Altere esta linha */}
+                    <div className="flex flex-col sm:flex-row items-start sm:items-center space-y-2 sm:space-y-0 sm:space-x-4 p-5 mb-2 border-b-2 border-gray-200">
+                      <img
+                        className="w-12 h-12 rounded-full"
+                        src={friend.friend.photoURL}
+                        alt="Friend avatar"
+                      />
+                      <div className="flex-1">
+                        <p className="text-black font-bold">
+                          {friend.friend.name}
+                        </p>
+                        <p className="text-gray-500 font-medium">
+                          {friend.friend.username}
+                        </p>
+                      </div>
+                    </div>
+                  </Link>
+                ))}
               </div>
               <button
                 onClick={onClose}
