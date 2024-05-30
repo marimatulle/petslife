02bb1906873d2d2ed3e6181b5fd89b928cbe4da1
Marina Matulle <marinamatulle.mm@gmail.com>
2024-05-29 21:13:36 -0300

    fix: adds validation and accessibility

--- src/register/Register.jsx
+++ src/register/Register.jsx
@@ -2,8 +2,16 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth, database } from "../firebase";
-import { setDoc, doc } from "firebase/firestore";
+import {
+  setDoc,
+  doc,
+  getDocs,
+  query,
+  where,
+  collection,
+} from "firebase/firestore";
 import { toast } from "react-toastify";
+import InputMask from "react-input-mask";
 
 import { ReactComponent as RegularUserSVG } from "../assets/pet-owners.svg";
 import { ReactComponent as VeterinarianSVG } from "../assets/vet.svg";
@@ -12,14 +20,55 @@ const Register = () => {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [fullName, setFullName] = useState("");
+  const [passwordError, setPasswordError] = useState("");
   const [username, setUsername] = useState("");
+  const [usernameError, setUsernameError] = useState("");
+  const [fullName, setFullName] = useState("");
   const [crmv, setCrmv] = useState("");
   const [userType, setUserType] = useState("");
   const [showButtons, setShowButtons] = useState(true);
 
+  const usernameExists = async (username) => {
+    const regularUsersQuery = query(
+      collection(database, "RegularUsers"),
+      where("username", "==", username)
+    );
+    const veterinariansQuery = query(
+      collection(database, "Veterinarians"),
+      where("username", "==", username)
+    );
+
+    const [regularUsersSnapshot, veterinariansSnapshot] = await Promise.all([
+      getDocs(regularUsersQuery),
+      getDocs(veterinariansQuery),
+    ]);
+
+    return !regularUsersSnapshot.empty || !veterinariansSnapshot.empty;
+  };
+
+  const validatePassword = (password) => {
+    if (password.length < 6) {
+      setPasswordError("A senha deve ter no mínimo 6 caracteres!");
+    } else {
+      setPasswordError("");
+    }
+  };
+
+  const validateUsername = async (username) => {
+    if (await usernameExists(username)) {
+      setUsernameError("Nome de usuário já existente!");
+    } else {
+      setUsernameError("");
+    }
+  };
+
   const handleRegister = async (e) => {
     e.preventDefault();
+    validatePassword(password);
+    await validateUsername(username);
+    if (passwordError || usernameError) {
+      return;
+    }
     try {
       await createUserWithEmailAndPassword(auth, email, password);
       const user = auth.currentUser;
@@ -63,7 +112,11 @@ const Register = () => {
       return (
         <div>
           <label className="text-lg font-medium">CRMV:</label>
-          <input
+          <span className="text-xs font-bold ml-1 text-gray-400">
+            UF-00000
+          </span>
+          <InputMask
+            mask="aa-99999"
             className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
             placeholder="Digite seu CRMV"
             onChange={(e) => setCrmv(e.target.value)}
@@ -124,9 +177,15 @@ const Register = () => {
                 <input
                   className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
                   placeholder="Digite seu nome de usuário"
-                  onChange={(e) => setUsername(e.target.value)}
+                  onChange={async (e) => {
+                    setUsername(e.target.value);
+                    await validateUsername(e.target.value);
+                  }}
                   required
                 />
+                {usernameError && (
+                  <p className="text-red-500">{usernameError}</p>
+                )}
               </div>
               <div>
                 <label className="text-lg font-medium">Email:</label>
@@ -143,9 +202,15 @@ const Register = () => {
                   className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
                   placeholder="Digite sua senha"
                   type="password"
-                  onChange={(e) => setPassword(e.target.value)}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                    validatePassword(e.target.value);
+                  }}
                   required
                 />
+                {passwordError && (
+                  <p className="text-red-500">{passwordError}</p>
+                )}
               </div>
               {renderForm()}
               <div className="mt-8 flex flex-col gap-y-4">
