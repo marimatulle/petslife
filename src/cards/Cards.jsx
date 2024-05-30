0d1d4633935f966ada06a1c8f14ddffac1cb9a36
Marina Matulle <marinamatulle.mm@gmail.com>
2024-05-30 15:47:53 -0300

    feat: implements the button to create a new card

--- src/cards/Cards.jsx
+++ src/cards/Cards.jsx
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { AiOutlinePlus } from "react-icons/ai";
 import Topbar from "../bars/Topbar";
+import CreateCardsModal from "./CreateCardsModal";
+import { auth, database } from "../firebase";
+import { doc, getDoc } from "firebase/firestore";
 
 const Cards = () => {
+  const [user, setUser] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    auth.onAuthStateChanged(async (user) => {
+      let docRef = doc(database, "RegularUsers", user.uid);
+      let docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        setUser(docSnap.data());
+      }
+    });
+  }, []);
+
+  const handleOpenModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <div className="bg-gray-100 min-h-screen">
       <Topbar location="/cards" />
+      {user && (
+        <button
+          onClick={handleOpenModal}
+          className="absolute right-0 m-5 bg-orange-300 hover:bg-orange-400 text-white rounded w-12 h-12 flex items-center justify-center text-2xl font-bold md:w-12 md:h-12 md:text-2xl" // Tornando o botão menor para computadores e posicionado à direita
+        >
+          <AiOutlinePlus />{" "}
+        </button>
+      )}
+      {showModal && <CreateCardsModal onClose={handleCloseModal} />}
     </div>
   );
 };
