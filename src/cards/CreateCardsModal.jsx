d9706197caa1d7a84550590d35b43c46c5e1d84c
Marina Matulle <marinamatulle.mm@gmail.com>
2024-05-31 14:59:52 -0300

    feat: implements the registration form for a new card

--- src/cards/CreateCardsModal.jsx
+++ src/cards/CreateCardsModal.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoClose } from "react-icons/io5";
+import { collection, addDoc } from "firebase/firestore";
+import { database } from "../firebase";
+import { toast } from "react-toastify";
 
 const CreateCardsModal = ({ onClose }) => {
+  const [animalName, setAnimalName] = useState("");
+  const [animalSpecies, setAnimalSpecies] = useState("");
+  const [animalBreed, setAnimalBreed] = useState("");
+  const [animalAge, setAnimalAge] = useState("");
+  const [animalColor, setAnimalColor] = useState("");
+  const [preExistingIllnesses, setPreExistingIllnesses] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const docRef = await addDoc(collection(database, "Cards"), {
+        animalName: animalName,
+        animalSpecies: animalSpecies,
+        animalBreed: animalBreed,
+        animalAge: animalAge,
+        animalColor: animalColor,
+        preExistingIllnesses: preExistingIllnesses,
+      });
+      toast.success("Carteira cadastrada com sucesso!", {
+        position: "top-center",
+      });
+      onClose();
+    } catch (error) {
+      console.error("Error adding document: ", error);
+      toast.error("Algum problema ocorreu no cadastro da carteira.", {
+        position: "bottom-center",
+      });
+    }
+  };
+
   return (
     <div
       className="fixed z-10 inset-0 overflow-y-auto"
@@ -26,8 +59,98 @@ const CreateCardsModal = ({ onClose }) => {
                   className="text-lg leading-6 font-bold text-gray-900"
                   id="modal-title"
                 >
-                  Criar nova carteira:
+                  Cadastrar nova carteira:
                 </h3>
+                <form onSubmit={handleSubmit}>
+                  <div className="mb-4">
+                    <label className="text-black font-medium">
+                      Nome do animal:
+                    </label>
+                    <input
+                      className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
+                      placeholder="Digite o nome do animal"
+                      type="text"
+                      value={animalName}
+                      onChange={(e) => setAnimalName(e.target.value)}
+                      required
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label className="text-black font-medium">
+                      Espécie do animal:
+                    </label>
+                    <select
+                      className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
+                      value={animalSpecies}
+                      onChange={(e) => setAnimalSpecies(e.target.value)}
+                      required
+                    >
+                      <option value="">Selecione a espécie</option>
+                      <option value="dog">Cachorro</option>
+                      <option value="cat">Gato</option>
+                    </select>
+                  </div>
+                  <div className="mb-4">
+                    <label className="text-black font-medium">
+                      Raça do animal:
+                    </label>
+                    <input
+                      className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
+                      placeholder="Digite a raça do animal"
+                      type="text"
+                      value={animalBreed}
+                      onChange={(e) => setAnimalBreed(e.target.value)}
+                      required
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label className="text-black font-medium">
+                      Idade do animal:
+                    </label>
+                    <input
+                      className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
+                      placeholder="Digite a idade do animal"
+                      type="text"
+                      value={animalAge}
+                      onChange={(e) => setAnimalAge(e.target.value)}
+                      required
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label className="text-black font-medium">
+                      Cor do animal:
+                    </label>
+                    <input
+                      className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
+                      placeholder="Digite a cor do animal"
+                      type="text"
+                      value={animalColor}
+                      onChange={(e) => setAnimalColor(e.target.value)}
+                      required
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label className="text-black font-medium">
+                      Doenças pré-existentes:
+                    </label>
+                    <h3 className="font-bold text-gray-400">
+                      Se o animal não possui nenhuma, apenas digite "Não"
+                    </h3>
+                    <input
+                      className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
+                      placeholder="Digite as doenças pré-existentes do animal"
+                      type="text"
+                      value={preExistingIllnesses}
+                      onChange={(e) => setPreExistingIllnesses(e.target.value)}
+                      required
+                    />
+                  </div>
+                  <div className="mt-8 flex flex-col gap-y-4">
+                    <button className="active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out py-3 rounded-xl bg-orange-400 text-white text-lg font-bold">
+                      Cadastrar
+                    </button>
+                  </div>
+                </form>
               </div>
               <button
                 onClick={onClose}
